eac3e2a3db50da2d6f52f5bf93f49e4c5d3c700d
Kinaj <jan.veverka177@gmail.com>
2026-06-24 20:12:15 +0200

    add Assets enum and dirs for audio

--- src/core/Assets.py
+++ src/core/Assets.py
@@ -1,5 +1,23 @@
 from enum import Enum
 
 class AssetsAudio(Enum):
-    
+    OTHER = "other"
+    # drawing music, blesk
+    NIGHT_EFFECTS = "night_effects"
+    # vrana, vlci (random efekt pri zvonech)
+    BELLS = "bells"
+    # zvuk zvonu pri zacatku noci
+    NIGHT_BACKGROUND = "night_background"
+    # dlouhe zvuky noci
+    DAY = "day"
+    # zvuk hodin, hlasovani?, zalar, poprava crowd, poprava chop
+    SCREAM_WOMAN = "scream_woman"
+    # random krik zeny
+    SCREAM_MAN = "scream_man"
+    # random krik muze
+    GOOD_WIN = "good_win"
+    # random good hudba
+    EVIL_WIN = "evil_win"
+    # random evil hudba
+
 
